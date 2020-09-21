33a7a63de048d9e54ade249d5c6e5a6a6e250bfc
Raffaele Ragni <raffaele.ragni@gmail.com>
2020-09-21 20:11:11 +0200

    added game options

--- src/components/campaign/CreateCampaign.vue
+++ src/components/campaign/CreateCampaign.vue
@@ -1,19 +1,11 @@
 <template>
   <div id="add-campaign">
-    Create new campaign with defaults:
+    Create new campaign:
     <input type="text" v-model="campaignName" placeholder="Campaign name"/>
-    <div v-for="(f, index) in factions" :key="index">
-      Faction:
-      <input type="text" v-model="f.faction" placeholder="Faction name to add to campaign"/>
-      Airbase:
-      <select v-model="f.airbase">
-        <option v-for="a in AIRBASES" :key="a" :value="a">{{a}}</option>
-      </select>
-      Coalition:
-      <select v-model="f.coalition">
-        <option v-for="a in COALITIONS" :key="a" :value="a">{{a}}</option>
-      </select>
-    </div>
+    <br/>
+    Campaign Options:<br/>
+    <textarea id="gameOptions" v-model="defaultGameOptions" rows="25" cols="80" :class="validOptions ? 'valid' : 'invalid'"></textarea>
+    <br/>
     <button @click="createCampaign">CREATE</button>
   </div>
 </template>
@@ -27,22 +19,33 @@ export default {
     return {
       AIRBASES: AIRBASES,
       COALITIONS: COALITIONS,
-      factions: [{coalition: "BLUE"}, {coalition: "BLUE"}, {coalition: "RED"}, {coalition: "RED"}],
       campaignName: "",
-      defaultGameOptions: {}
+      defaultGameOptions: "",
+      validOptions: false
     }
   },
   methods: {
     createCampaign() {
-      var factionsToAdd = this.factions.filter(i => i.faction && i.airbase);
       getDefaultGameOptions(opt => {
         createCampaign({
           name: this.campaignName,
-          factions: factionsToAdd,
           gameOptions: opt
         }, () => this.$parent.reloadCampaigns());
       });
     }
+  },
+  watch: {
+    defaultGameOptions() {
+      try {
+        JSON.parse(this.defaultGameOptions)
+        this.validOptions = true
+      } catch (e) {
+        this.validOptions = false
+      }
+    }
+  },
+  created() {
+    getDefaultGameOptions(opt => this.defaultGameOptions = JSON.stringify(opt, null, 2));
   }
 }
 </script>
@@ -50,5 +53,11 @@ export default {
 <style>
 #add-campaign {
   border: solid 1px black;
+}
+#gameOptions.valid {
+ color: green;
+}
+#gameOptions.invalid {
+  color: red;
 }
 </style>
