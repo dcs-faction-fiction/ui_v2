1009a1a088859fc9119cefcc969ee03ceeeab5c6
Raffaele Ragni <raffaele.ragni@gmail.com>
2020-09-30 17:31:11 +0200

    use labels on map

--- src/components/faction/UnitMap.vue
+++ src/components/faction/UnitMap.vue
@@ -18,7 +18,12 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
   useCache: true
 });
 Esri_WorldImagery.addTo(map)
-
+var CartoDB_PositronOnlyLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+  maxZoom: 19
+});
+CartoDB_PositronOnlyLabels.addTo(map)
 }
 
 export default {
