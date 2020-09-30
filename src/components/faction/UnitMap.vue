2bab8cc391c5716a00ed4a0479e14c10a102aad1
Raffaele Ragni <raffaele.ragni@gmail.com>
2020-09-30 17:38:10 +0200

    change map labels

--- src/components/faction/UnitMap.vue
+++ src/components/faction/UnitMap.vue
@@ -18,12 +18,14 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
   useCache: true
 });
 Esri_WorldImagery.addTo(map)
-var CartoDB_PositronOnlyLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+var Stamen_TonerLabels = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
-  maxZoom: 19
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
 });
-CartoDB_PositronOnlyLabels.addTo(map)
+Stamen_TonerLabels.addTo(map)
 }
 
 export default {
